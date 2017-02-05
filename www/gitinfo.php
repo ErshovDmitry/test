e848bec665b524d76b9df4a3a186b981f8d8f787
Ершов Дмитрий <Dmitry@dima.muska.org>
2017-02-05 15:22:18 +0700

    Добавил файл gitinfo.php

---
+++ www/gitinfo.php
@@ -0,0 +1,6 @@
+<?php
+echo "git clone - Клонирование";
+echo "git add - Добавить файл под контроль git";
+echo "git commit -m "Сообщение" - фиксируем изменения в истории";
+echo "git push - отправим локальные изменения на github";
+echo "git pull - забрать изменения с github";
